1a65758c691a099a46fd11173c0f5d399946ebc0
Andrea Celletti <celletti.andrea87@gmail.com>
2024-04-01 09:39:40 +0100

    feat: Add support for MDX using `@mdx-js/rollup` plugin

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
-    Nuxt module playground!
+    <NuxtPage />
   </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,30 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
-
+import { defineNuxtModule, addPlugin, createResolver } from "@nuxt/kit";
+import mdx from "@mdx-js/rollup";
+import { type Plugin as VitePlugin } from "vite";
 // Module options TypeScript interface definition
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule'
+    name: "@nuxt/content",
+    configKey: "mdx",
   },
   // Default configuration options of the Nuxt module
   defaults: {},
-  setup (options, nuxt) {
-    const resolver = createResolver(import.meta.url)
+  setup(options, nuxt) {
+    const resolver = createResolver(import.meta.url);
+
+    if (!nuxt.options.extensions.includes(".mdx"))
+      nuxt.options.extensions.push(".mdx");
+
+    nuxt.hook("vite:extendConfig", (config) => {
+      config.plugins?.push({
+        enforce: "pre",
+        ...mdx({ jsxImportSource: "vue" }),
+      } as VitePlugin);
+    });
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
-  }
-})
+    addPlugin(resolver.resolve("./runtime/plugin"));
+  },
+});
